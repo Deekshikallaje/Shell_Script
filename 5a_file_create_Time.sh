2d0f07a86b304c880e1f279e0aac8d348884929f
sibinsunny404 <sibinsunny33@gmail.com>
2022-02-24 19:35:19 -0800

    5a program commit

--- 5a_file_create_Time.sh
+++ 5a_file_create_Time.sh
@@ -1,3 +1,4 @@
+#Write a shell script that accepts filename as argument and display its creation time if file exit and if does not send output error message.
 if [ $# -eq 0 ]
 then
 echo "argument is not enterd"
